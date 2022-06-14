202a4b191d8411023f165e654e54c355907e5232
Sandro Jk <sandro007@gmail.com>
2022-06-14 17:00:29 -0300

    OK

--- MAI/ANALISE_CAMPANHAS_0522.R
+++ MAI/ANALISE_CAMPANHAS_0522.R
@@ -16,6 +16,7 @@ library(scales)
 con2 <- dbConnect(odbc::odbc(), "reproreplica")
 
 
+## GERAL
 
 RESULT_GERAL <- read_sheet("1TIlc2UCW9Cz1wVyXe9Fp6WgopDOROJfDkdJUPSWwxek",
                              sheet = "RESUMODADOS") 
@@ -36,11 +37,11 @@ RESULT_GERAL %>% mutate(PROP=percent(BONUS/sum(BONUS)))%>%
         panel.grid = element_blank(),
         legend.position = "top",
         text = element_text(size=15)
-  ) + scale_fill_manual(values=c("#1b85b8","#5a5255","#559e83","#ae5a41"))
+  ) + scale_fill_manual(values=c("#275f96","#328c62","#db884c","#8d8d8d",))
 
 
 
-## GET CPF
+## INSIGNE 
 
 RESULT_INSIGNE <- read_sheet("1TIlc2UCW9Cz1wVyXe9Fp6WgopDOROJfDkdJUPSWwxek",
                        sheet = "INSIGNE") 
@@ -50,7 +51,7 @@ RESULT_INSIGNE %>% ggplot(.,aes(DATE,VALOR,fill=DATE)) + geom_bar(stat = "identi
   geom_text(aes(label=format(VALOR,big.mark=","),vjust=-0.1)) + 
   scale_x_datetime(date_breaks = "month",date_labels = "%b/%Y",expand = c(0.01,0.01)) +
   scale_y_continuous(expand = c(0,0),limits = c(0,22000)) + 
-  scale_fill_datetime(low="#cce5e5",high = "#004c4c") +
+  scale_fill_datetime(low="#b7c9dc",high = "#092e53") +
   theme(axis.text.x = element_text(size = 14),
                       axis.title.x = element_blank(),
                       axis.title.y = element_blank(),
@@ -58,9 +59,10 @@ RESULT_INSIGNE %>% ggplot(.,aes(DATE,VALOR,fill=DATE)) + geom_bar(stat = "identi
         axis.ticks.y = element_blank(),
         panel.grid = element_blank(),
         legend.position = "none"
-        )
+        ) + labs(title = "Bonificações Insigne")
 
 
+## REBATES
 
 RESULT_REBATES <- read_sheet("1TIlc2UCW9Cz1wVyXe9Fp6WgopDOROJfDkdJUPSWwxek",
                              sheet = "REBATES") 
@@ -70,7 +72,7 @@ RESULT_REBATES %>% ggplot(.,aes(DATE,VALOR,fill=DATE)) + geom_bar(stat = "identi
   geom_text(aes(label=format(VALOR,big.mark=","),vjust=-0.1)) + 
   scale_x_datetime(date_breaks = "month",date_labels = "%b/%Y",expand = c(0.01,0.01)) +
   scale_y_continuous(expand = c(0,0),limits = c(0,22000)) + 
-  scale_fill_datetime(low="#ffd9d4",high = "#b22e1d") +
+  scale_fill_datetime(low="#eabb99",high = "#a34400") +
   theme(axis.text.x = element_text(size = 13),
         axis.title.x = element_blank(),
         axis.title.y = element_blank(),
@@ -78,8 +80,10 @@ RESULT_REBATES %>% ggplot(.,aes(DATE,VALOR,fill=DATE)) + geom_bar(stat = "identi
         axis.ticks.y = element_blank(),
         panel.grid = element_blank(),
         legend.position = "none"
-  ) + labs(title = "Rebates", subtitle = "bonificações mensais")
+  ) + labs(title = "Bonificações Rebates")
 
+
+## OUTRAS CAMPANHAS
 
 OUTRAS_CAMPANHAS <- read_sheet("1TIlc2UCW9Cz1wVyXe9Fp6WgopDOROJfDkdJUPSWwxek",
                              sheet = "OUTRAS CAMPANHAS") 
@@ -88,8 +92,8 @@ OUTRAS_CAMPANHAS <- read_sheet("1TIlc2UCW9Cz1wVyXe9Fp6WgopDOROJfDkdJUPSWwxek",
 OUTRAS_CAMPANHAS %>% ggplot(.,aes(DATE,VALOR,fill=DATE)) + geom_bar(stat = "identity") + 
   geom_text(aes(label=format(VALOR,big.mark=","),vjust=-0.1)) + 
   scale_x_datetime(date_breaks = "month",date_labels = "%b/%Y",expand = c(0.01,0.01)) +
-  scale_y_continuous(expand = c(0,0),limits = c(0,22000)) + 
-  scale_fill_datetime(low="#cdddd6",high = "#043b25") +
+  scale_y_continuous(expand = c(0,0),limits = c(0,10000)) + 
+  scale_fill_datetime(low="#99c5b1",high = "#004e2a") +
   theme(axis.text.x = element_text(size = 14),
         axis.title.x = element_blank(),
         axis.title.y = element_blank(),
@@ -97,7 +101,10 @@ OUTRAS_CAMPANHAS %>% ggplot(.,aes(DATE,VALOR,fill=DATE)) + geom_bar(stat = "iden
         axis.ticks.y = element_blank(),
         panel.grid = element_blank(),
         legend.position = "none"
-  )  + labs(title = "Outras Campanhas")
+  )  + labs(title = "Bonificações em Outras Campanhas")
+
+
+## RETROATIVOS
 
 
 RETROATIVOS <- read_sheet("1TIlc2UCW9Cz1wVyXe9Fp6WgopDOROJfDkdJUPSWwxek",
@@ -107,8 +114,8 @@ RETROATIVOS <- read_sheet("1TIlc2UCW9Cz1wVyXe9Fp6WgopDOROJfDkdJUPSWwxek",
 RETROATIVOS %>% ggplot(.,aes(DATE,VALOR,fill=DATE)) + geom_bar(stat = "identity") + 
   geom_text(aes(label=format(VALOR,big.mark=","),vjust=-0.1)) + 
   scale_x_datetime(date_breaks = "month",date_labels = "%b/%Y",expand = c(0.01,0.01)) +
-  scale_y_continuous(expand = c(0,0),limits = c(0,22000)) + 
-  scale_fill_datetime(low="#f0c1c1",high = "#7b1e1e") +
+  scale_y_continuous(expand = c(0,0),limits = c(0,15000)) + 
+  scale_fill_datetime(low="#b3b3b3",high = "#1a1a1a") +
   theme(axis.text.x = element_text(size = 14),
         axis.title.x = element_blank(),
         axis.title.y = element_blank(),
@@ -116,4 +123,4 @@ RETROATIVOS %>% ggplot(.,aes(DATE,VALOR,fill=DATE)) + geom_bar(stat = "identity"
         axis.ticks.y = element_blank(),
         panel.grid = element_blank(),
         legend.position = "none"
-  ) + labs(title = "Retroativos")
+  ) + labs(title = "Bonificações Retroativos")
