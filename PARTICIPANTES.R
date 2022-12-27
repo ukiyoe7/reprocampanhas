0cd6d5c35b9e02511dabc2a0edf4537fcfe37e4f
Sandro Jk <sandro007@gmail.com>
2022-12-27 18:17:09 -0300

    OK

--- PARTICIPANTES.R
+++ PARTICIPANTES.R
@@ -1,13 +1,18 @@
-
-## PARTICIPANTES
+## PARTICIPANTES CAMPANHAS
 
 
 library(DBI)
 library(dplyr)
 library(googlesheets4)
-con2 <- dbConnect(odbc::odbc(), "reproreplica")
-## CAMPANHAS INSIGNE 
+library(googledrive)
 
+con2 <- dbConnect(odbc::odbc(), "reproreplica")
+
+
+## VERIFICAR REGSTRO PARTICIPANTE ================================================
+
+
+drive_get(id="1jUVGD4qsU0ZI7Z9Tgo8in_82KD_GA4xlseQs1gQPdCQ")
 
 
 PARTICIPANTES_CAMPANHA <- read_sheet("1jUVGD4qsU0ZI7Z9Tgo8in_82KD_GA4xlseQs1gQPdCQ",
@@ -16,83 +21,45 @@ PARTICIPANTES_CAMPANHA <- read_sheet("1jUVGD4qsU0ZI7Z9Tgo8in_82KD_GA4xlseQs1gQPd
 
 View(PARTICIPANTES_CAMPANHA)
 
+
+## VERIFICA SE ESTA NA BASE DE CLIENTES  ================================================
+
+
+partic_dados <- left_join(cli %>% 
+                             filter(GCLCODIGO==82),PARTICIPANTES_CAMPANHA,by="CLICODIGO") %>% 
+                               mutate(CPF=as.character(CPF)) %>% .[c(1,2,6,7,11:13)]
+
+
+View(partic_dados)
+
+
+## VERIFICA SE ESTA BASE DE CLIENTES CAMPANHAS  ================================================
+
 BASE_CPF <- read_sheet("1ShpVwae6DVAqYW3afQli7XggL_7cJrDiWeKw2luKpC0",
-                       sheet = 'DADOS') %>% select(CLICODIGO,CPF) 
-
-View(BASE_CPF)
+                       sheet = 'DADOS') 
 
 
-
-CAMPANHAS <- read_sheet("1Tt7VLY1oHoirHduSaJoj62SlkyZCMVIZ9eHzLAhpqIY",
-                       sheet = 'CAMPANHAS ATIVAS') %>% .[,c(2,3,4,5,6,7,8,13,14,15)] 
-
-View(CAMPANHAS)
-
-
-## BASE CPF
-
-left_join(BASE_CPF,cli,by="CLICODIGO") %>% View()
+left_join(partic_dados %>%  mutate(CPF=as.character(CPF)),BASE_CPF %>% 
+            mutate(CPF=as.character(CPF)),by="CLICODIGO") %>% View()
 
 
 
-## CAMPANHAS LOJAS
+## VERIFICA SE HA CARTAO ================================================  
 
-CAMPANHAS_LOJAS <- read_sheet("1Tt7VLY1oHoirHduSaJoj62SlkyZCMVIZ9eHzLAhpqIY",
-                               sheet = 'CAMPANHAS ATIVAS') %>% .[,c(2,4,6,7,8,12,13,14,15)] %>% 
-  filter(!is.na(CLICODIGO))
+CARTOES <- CARTOES_1222 %>%   
+            mutate(CPF=sub("\\D+", '',CPF)) %>% 
+              mutate(CPF=sub("\\.", '',CPF)) %>% 
+               mutate(CPF=sub("\\-", '',CPF)) %>% 
+                mutate(CPF=sub("\\,", '',CPF)) 
 
+CARTOES <- inner_join(partic_dados,CARTOES,by="CPF")
 
-cli <- dbGetQuery(con2,"SELECT DISTINCT C.CLICODIGO,
-                          CLINOMEFANT,
-                           ENDCODIGO,
-                            C.GCLCODIGO,
-                             GCLNOME,
-                              SETOR
-                               FROM CLIEN C
-                                LEFT JOIN (SELECT CLICODIGO,E.ZOCODIGO,ZODESCRICAO SETOR,ENDCODIGO FROM ENDCLI E
-                                 LEFT JOIN (SELECT ZOCODIGO,ZODESCRICAO FROM ZONA WHERE ZOCODIGO IN (20,21,22,23,24,25,28))Z ON E.ZOCODIGO=Z.ZOCODIGO WHERE ENDFAT='S')A ON C.CLICODIGO=A.CLICODIGO
-                                  LEFT JOIN GRUPOCLI GR ON C.GCLCODIGO=GR.GCLCODIGO
-                                   WHERE CLICLIENTE='S'")
-
-
-left_join(CAMPANHAS_LOJAS ,cli,by="CLICODIGO") %>% 
-          left_join(.,PARTICIPANTES_CAMPANHA,by="CLICODIGO") %>% 
-            left_join(.,BASE_CPF,by="CLICODIGO") %>% View()
+View(CARTOES)
 
 
 
 
 
-## CAMPANHAS GRUPOS
 
-CAMPANHAS_GRUPOS <- read_sheet("1Tt7VLY1oHoirHduSaJoj62SlkyZCMVIZ9eHzLAhpqIY",
-                        sheet = 'CAMPANHAS ATIVAS') %>% .[,c(3,4,5,6,7,8,12,13,14,15)] %>% 
-                         filter(!is.na(GCLCODIGO))
-
-
-cli <- dbGetQuery(con2,"SELECT DISTINCT C.CLICODIGO,
-                          CLINOMEFANT,
-                           ENDCODIGO,
-                            C.GCLCODIGO,
-                             GCLNOME,
-                              SETOR
-                               FROM CLIEN C
-                                LEFT JOIN (SELECT CLICODIGO,E.ZOCODIGO,ZODESCRICAO SETOR,ENDCODIGO FROM ENDCLI E
-                                 LEFT JOIN (SELECT ZOCODIGO,ZODESCRICAO FROM ZONA WHERE ZOCODIGO IN (20,21,22,23,24,25,28))Z ON E.ZOCODIGO=Z.ZOCODIGO WHERE ENDFAT='S')A ON C.CLICODIGO=A.CLICODIGO
-                                  LEFT JOIN GRUPOCLI GR ON C.GCLCODIGO=GR.GCLCODIGO
-                                   WHERE CLICLIENTE='S'")
-
-
-left_join(CAMPANHAS_GRUPOS ,cli,by="GCLCODIGO") %>% left_join(.,PARTICIPANTES_CAMPANHA,by="CLICODIGO") %>% View()
-
-filter
-g37 <- cli %>% filter(GCLCODIGO==37)
-  
-  left_join(CAMPANHAS_GRUPOS ,cli,by="GCLCODIGO") %>% 
-          left_join(.,PARTICIPANTES_CAMPANHA,by="CLICODIGO") %>% filter(GCLCODIGO==37)
-
-
-range_write("1MvPuAKK2IpluVbkB65sfbaJGqXvlRRPUXFbwpf1b4Q4",data=g37,sheet = "DADOS",
-            range = "A1",reformat = FALSE)
 
 
